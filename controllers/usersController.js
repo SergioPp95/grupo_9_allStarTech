dd0278e768e2119b6137563e37e1e664a86a22ec
Sanchezedg14 <sanchezedgar_3@hotmail.com>
2022-10-31 17:58:58 -0500

    Modificacion de rutas para pagina Crud

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,7 +2,6 @@ const controller = {
     login: (req,res) => res.render('./users/login'),
     register: (req,res) => res.render('./users/register'),
     cart: (req,res) => res.render('./users/productCart'),
-    crud: (req,res) => res.render('./users/crud')
 }
 
 module.exports = controller;
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const indexController = require('../controllers/indexController')
 
 router.get('/',indexController.index)
+router.get('/crud', indexController.crud)
 
 module.exports = router;
