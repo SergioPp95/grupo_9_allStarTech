43c5347b215fd938b185d395089adceea2c48221
Alberto Ortiz <baortiz98@outlook.com>
2022-11-17 12:54:34 -0500

    se cambió la dirección de las imagenes para mejorar organización

--- src/routes/product.js
+++ src/routes/product.js
@@ -8,7 +8,7 @@ const path = require("path");
 // ** Multer **
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images')
+        cb(null, './public/images/products')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
