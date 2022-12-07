b2b284fb2fba78d34786d1d0ef0259949d3d09c5
nico-lanzilotta <nicolanzilotta@hotmail.com>
2022-12-07 20:23:17 -0300

    validacion crud

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { validationResult } = require('express-validator');
 
 const productsPath = path.join(__dirname, '../data/products.json')
 let products = JSON.parse(fs.readFileSync(productsPath, 'utf-8'))
@@ -12,21 +13,31 @@ const controller = {
     res.render('./products/product-create')
   },
   store: (req, res) => {
-    let product = {
-      id: Date.now(),
-      name: req.body.name,
-      description: req.body.description,
-      imageMain: req.files.image1[0].filename,
-      imageOther: req.files.image2[0].filename,
-      category: req.body.category,
-      price: req.body.price,
-      discount: req.body.discount,
+     const resultValidation = validationResult(req);
+		
+		if (resultValidation.errors.length > 0) {
+			return res.render('./products/product-Create', {
+				errors: resultValidation.mapped(),
+				oldData: req.body
+			});
+		} else{
+
+      let product = {
+        id: Date.now(),
+        name: req.body.name,
+        description: req.body.description,
+        imageMain: req.files.image1[0].filename,
+        imageOther: req.files.image2[0].filename,
+        category: req.body.category,
+        price: req.body.price,
+        discount: req.body.discount,
+      }
+
+      products.push(product)
+      fs.writeFileSync(productsPath, JSON.stringify(products, null, " "));
+
+      res.redirect('/products/')
     }
-
-    products.push(product)
-    fs.writeFileSync(productsPath, JSON.stringify(products, null, " "));
-
-    res.redirect('/products/')
   },
   detail: (req, res) => {
     let id = req.params.id
@@ -41,30 +52,40 @@ const controller = {
 
     res.render('./products/product-edit', { product })
   },
-  update: (req, res) => {
+  update: (req, res) => { 
+     const resultValidation = validationResult(req);
+		
+		if (resultValidation.errors.length > 0) {
+			return res.render('./products/product-Create', {
+				errors: resultValidation.mapped(),
+				oldData: req.body
+			});
+		} else {
 
-   // Filtra producto a editar
-   const product = products.find(element => element.id == req.params.id)
-   
-   // Elimina imagenes anteriores del producto
-   req.files.image1 ? fs.unlinkSync(path.join(__dirname, "../../public/images/products", product.imageMain)) : null
-   req.files.image2 ? fs.unlinkSync(path.join(__dirname, "../../public/images/products", product.imageOther)) : null
 
-   // Asigna nuevos valores a cada atributo
-   product.id = req.params.id
-   product.name = req.body.name
-   product.description = req.body.description
-   product.imageMain = req.files.image1 ? req.files.image1[0].filename : product.imageMain
-   product.imageOther = req.files.image2 ? req.files.image2[0].filename : product.imageOther
-   product.category = req.body.category
-   product.price = req.body.price
-   product.discount = req.body.discount
+    // Filtra producto a editar
+    const product = products.find(element => element.id == req.params.id)
+    
+    // Elimina imagenes anteriores del producto
+    req.files.image1 ? fs.unlinkSync(path.join(__dirname, "../../public/images/products", product.imageMain)) : null
+    req.files.image2 ? fs.unlinkSync(path.join(__dirname, "../../public/images/products", product.imageOther)) : null
 
-    // Reescribe archivo json
-    fs.writeFileSync(productsPath, JSON.stringify(products, null, " "));
+    // Asigna nuevos valores a cada atributo
+    product.id = req.params.id
+    product.name = req.body.name
+    product.description = req.body.description
+    product.imageMain = req.files.image1 ? req.files.image1[0].filename : product.imageMain
+    product.imageOther = req.files.image2 ? req.files.image2[0].filename : product.imageOther
+    product.category = req.body.category
+    product.price = req.body.price
+    product.discount = req.body.discount
 
-    // Reenvia a página del producto recién editado
-    res.redirect('/products/' + req.params.id)
+      // Reescribe archivo json
+      fs.writeFileSync(productsPath, JSON.stringify(products, null, " "));
+
+      // Reenvia a página del producto recién editado
+      res.redirect('/products/' + req.params.id)
+    }
   },
   delete: (req, res) => {
     
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router();
+const validations = require('../middlewares/createEditProductValidation')
 
 const productController = require('../controllers/productController');
 
 const upload = require("../middlewares/uploadProduct")
+
+
 
 // ** Rutas **
 router.get('/', productController.index);
@@ -13,7 +16,7 @@ router.get('/create', productController.create); // Formulario de crear producto
 router.post('/', upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 }
-]), productController.store); // Recibe datos de creacion
+]), validations, productController.store); // Recibe datos de creacion
 
 router.get('/:id', productController.detail); // Muestra detalles
 
@@ -22,7 +25,7 @@ router.get('/:id/edit', productController.edit) // Formulario de editar producto
 router.put('/:id', upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 }
-]), productController.update) // Recibe datos de edicion
+]), validations, productController.update) // Recibe datos de edicion
 
 router.delete('/:id', productController.delete)
 
