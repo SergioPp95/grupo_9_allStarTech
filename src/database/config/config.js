eabf92455e00511347c954f4ab59612547dbff36
Alberto Ortiz <65422300+tizor98@users.noreply.github.com>
2023-01-08 22:35:49 -0500

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "12345",
+    "password": null,
     "database": "allstartech",
     "host": "127.0.0.1",
     "dialect": "mysql"
