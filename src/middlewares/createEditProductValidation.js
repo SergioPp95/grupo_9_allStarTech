367e8fefca8056d5ec7aefbd1f70e204c1645d6b
JuanmaZimm <juanmanuelzimmermann@gmail.com>
2023-01-31 15:06:05 -0300

    cambios productos

--- src/middlewares/createEditProductValidation.js
+++ src/middlewares/createEditProductValidation.js
@@ -2,14 +2,32 @@ const path = require("path");
 const { check } = require('express-validator') //req validator
 
 const validations = [
-	check('name').notEmpty().custom((value) => {
-        if (value.length < 5) {
-            throw new Error('El nombre es obligatiorio (5 caracteres obligatorio)')
-        }
-        return true;
-    }),
-	check('price').notEmpty().withMessage('Tienes que insertar el precio del producto'),
-	check('discount').notEmpty().withMessage('Tienes que insertar el descuento del producto'),
+	check('name').custom((value) => {
+		if (value.length < 5) {
+			throw new Error('El nombre es obligatiorio (5 caracteres obligatorios)')
+		}
+		return true;
+	}),
+	check('price').notEmpty().withMessage('Tienes que insertar el precio del producto')
+		.custom((value) => {
+			const regex = /^\d+$/
+			let valid = regex.test(value)
+			if (!valid) {
+				throw new Error('Valor no válido');
+			}
+			return true
+		}),
+	check('discount').custom((value) => {
+		if (value) {
+			const regex = /^\d+$/
+			let valid = regex.test(value)
+			if (!valid || value > 100 || value < 0) {
+				throw new Error('Valor no válido');
+			}
+		}
+
+		return true
+	}),
 	check('category').notEmpty().withMessage('Tienes que elegir la categoria del producto'),
 	check('image1').custom(function (value, { req }) {
 		if (req.files.image1) {
@@ -22,7 +40,7 @@ const validations = [
 		return true
 	}),
 	check('image2').custom(function (value, { req }) {
-		if (req.files.image1) {
+		if (req.files.image2) {
 			let extension = (path.extname(req.files.image2[0].originalname)).toLowerCase();
 			if (!(['.jpg', '.png', '.jpeg'].includes(extension))) {
 				throw new Error('Tienes que subir una imagen en formato:'['.jpg', '.png', '.jpeg'].join(', '))
@@ -32,11 +50,11 @@ const validations = [
 		return true
 	}),
 	check('description').custom((value) => {
-        if (value && value.length < 20) {
-            throw new Error('La descripción debe contener como minimo 20 caracteres')
-        }
-        return true;
-    })
+		if (value && value.length < 20) {
+			throw new Error('La descripción debe contener como minimo 20 caracteres')
+		}
+		return true;
+	})
 ]
 
 module.exports = validations
